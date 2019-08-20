07a63da9ea9486ff3c2900d1cd2a5cb3740fc56b
Chase Hanson <Hallsville3@gmail.com>
2019-08-20 11:29:15 -0500

    Added [-]: data[ptr] = 0 op and support for other optimizations

--- src/Compiler.py
+++ src/Compiler.py
@@ -1,6 +1,6 @@
 def clean(bf):
-    # Removes any non-bf characters from the given program
-    return ''.join(char for char in bf if char in "+-<>.,[]")
+    # Removes any non-bf characters from the given program and returns as a list
+    return [char0 for char0 in bf if char0 in "+-<>.,[]"]
 
 
 class Compiler(object):
@@ -24,49 +24,56 @@ class Compiler(object):
         sum_balance = 0
         location_balance = 0
 
-        for index in range(len(self.bf)):
-            char = self.bf[index]
-            next_char = "\0" if index + 1 == len(self.bf) else self.bf[index + 1]
-
-            if char in '+-':
+        index = 0
+        while index < len(self.bf):
+            char0 = self.bf[index]
+            char1 = "\0" if index + 1 == len(self.bf) else self.bf[index + 1]
+            char2 = "\0" if index + 2 >= len(self.bf) else self.bf[index + 2]
+            print "hi"
+            if char0 + char1 + char2 == "[-]":
+                self.compiled.append(indent * indent_count + "data[ptr] = 0")
+                index += 2
+            elif char0 in '+-':
                 #This section condenses consecutive +- chars into one line of python
-                if char == '+':
+                if char0 == '+':
                     sum_balance += 1
                 else:
                     sum_balance -= 1
-                if next_char not in '+-' and sum_balance != 0:
-                    #This means that char is the last consecutive + or -
+                if char1 not in '+-' and sum_balance != 0:
+                    #This means that char0 is the last consecutive + or -
                     if sum_balance > 0:
                         self.compiled.append(indent * indent_count + "data[ptr] += {}".format(sum_balance))
                     else:
                         self.compiled.append(indent * indent_count + "data[ptr] -= {}".format(abs(sum_balance)))
                     sum_balance = 0
 
-            elif char in '<>':
+            elif char0 in '<>':
                 #This section condenses consecutive <> chars into one line of python
-                if char == '>':
+                if char0 == '>':
                     location_balance += 1
                 else:
                     location_balance -= 1
-                if next_char not in '<>' and location_balance != 0:
-                    #This means that char is the last consecutive < or >
+                if char1 not in '<>' and location_balance != 0:
+                    #This means that char0 is the last consecutive < or >
                     if location_balance > 0:
                         self.compiled.append(indent * indent_count + "ptr += {}".format(location_balance))
                     else:
                         self.compiled.append(indent * indent_count + "ptr -= {}".format(abs(location_balance)))
                     location_balance = 0
 
-            elif char == '.':
+            elif char0 == '.':
                 self.compiled.append(indent * indent_count + "print(data[ptr])")
 
-            elif char == ',':
+            elif char0 == ',':
                 self.compiled.append(indent * indent_count + "data[ptr] = input('Slot {}: '.format(ptr))")
 
-            elif char == '[':
+            elif char0 == '[':
                 self.compiled.append(indent * indent_count + "while data[ptr] != 0:")
                 indent_count += 1
 
-            elif char == ']':
+            elif char0 == ']':
                 indent_count -= 1
+
+            index += 1
 
         self.compiled = "\n".join(self.compiled)
