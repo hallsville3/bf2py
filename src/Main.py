1900abf8d5e69d94d6f241578396ae691db8c308
Chase Hanson <hallsville3@gmail.com>
2020-02-23 22:26:29 -0600

    update to python3

--- src/Main.py
+++ src/Main.py
@@ -36,5 +36,5 @@ optimization_level = 2
 compiler = Compiler("+++++[->+>+<<]>>[-<<+>>]")
 compiler.compile(op_level=optimization_level, cap=0)
 
-print compiler.compiled
-print compiler.log
+print(compiler.compiled)
+print(compiler.log)
