1b9ccadeb9f40344580e77d64fbcf23d91e00248
Chase Hanson <hallsville3@gmail.com>
2020-03-19 10:03:47 -0500

    Make optimization_level an optional argument. Default 2.

--- src/main.py
+++ src/main.py
@@ -31,12 +31,16 @@ Cap
     An optional parameter that allows the user to cap the value of the cells
 """
 
-if (len(sys.argv) < 3):
+if (len(sys.argv) < 2):
     print(f"Usage: python3 {sys.argv[0]} filename optimization_level")
     sys.exit(1)
 
 compiler = Compiler(sys.argv[1])
-optimization_level = int(sys.argv[2])
+
+if (len(sys.argv) > 2):
+    optimization_level = int(sys.argv[2])
+else:
+    optimization_level = 2
 
 compiler.compile(op_level=optimization_level, cap=0)
 
